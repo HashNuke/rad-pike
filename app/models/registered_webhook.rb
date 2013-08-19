c1e9abcee28a985807c718581dcf95295d579c8d
Akash Manohar J <akash@akash.im>
2013-08-19 21:33:40 +0530

    Add validation for webhooks

--- app/models/registered_webhook.rb
+++ app/models/registered_webhook.rb
@@ -1,3 +1,9 @@
 class RegisteredWebhook < ActiveRecord::Base
   has_many :registered_webhook_events
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :url,  format: {
+      with: /\Ahttp\:s?\/\/.+\z/,
+      message: "should start with http:// or https:// and should be valid"
+    }
 end
