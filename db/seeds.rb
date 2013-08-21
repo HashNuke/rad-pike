9203ecc8acb4eca02700ac4dedcbe08798460fc0
Akash Manohar J <akash@akash.im>
2013-08-21 10:50:16 +0530

    Better name for first admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+puts "Creating issue state types..."
 ["Resolved", "Unresolved", "Unknown"].each do |issue_state_type|
   IssueStateType.find_or_create_by_name issue_state_type
 end
 
-
+puts "Creating user roles..."
 ["Admin", "Staff", "Deactivated Staff", "Customer", "Guest"].each do |role_name|
   Role.find_or_create_by_name role_name
 end
 
+puts "Creating first admin user"
 User.find_or_create_by_email("admin@example.com",
   password: "password",
   role_id:  Role.admin.id,
-  name: "Support Tom")
+  name: "Support Superhuman")
