7e59f4882461cb8f4ffb4444e8006ef2f798f691
Akash Manohar J <akash@akash.im>
2013-08-21 09:37:13 +0530

    Remove redis stuff from AppConfig

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -12,23 +12,4 @@ class AppConfig
     @toml ||= ::TOML.load_file(config_file)
   end
 
-  def self.redis_opts
-    return @redis_opts if !@redis_options.nil?
-
-    @redis_opts = {host: values["redis"]["host"]}
-
-    unless values["redis"]["port"].empty?
-      @redis_opts.merge!(port: values["redis"]["port"])
-    end
-
-    unless values["redis"]["password"].empty?
-      @redis_opts.merge!(port: values["redis"]["password"])
-    end
-    @redis_opts
-  end
-
-  def self.redis
-    @redis ||= Redis.new(redis_opts)
-  end
-
 end
