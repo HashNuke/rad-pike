d7a4a155fac191c81ede06016517ba7720fb4b9f
Akash Manohar J <akash@akash.im>
2013-08-21 11:13:09 +0530

    Fix asset compilation issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ RadPike::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(mangle: false) if defined? Uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
