b1386909f178992bed33369260648c07350afc64
Akash Manohar J <akash@akash.im>
2013-08-22 11:47:55 +0530

    Add attribute to serializer to check if user belongs to support team

--- app/serializers/public_user_serializer.rb
+++ app/serializers/public_user_serializer.rb
@@ -1,3 +1,7 @@
 class PublicUserSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :is_support_team
+
+  def is_support_team
+    object.support_team?
+  end
 end 
